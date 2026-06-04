33af3032afb9c7ba5a825f59884363f5c55de82b
Luque Marcos <61090242+Marcos564@users.noreply.github.com>
2026-06-04 10:24:02 -0300

    Update esquemas.py

--- esquemas.py
+++ esquemas.py
@@ -11,12 +11,25 @@ class EscenarioOut(BaseModel):
 
 class DatosGeneralesOut(BaseModel):
     Total_Perifericos:        int
+    Cantidad_Mouses:          int
+    Cantidad_Teclados:        int
+    
     Ingreso_Bruto_USD:        float
     Tiempo_Total_Horas:       float
     Material_Reutilizable_gr: float
     Residuo_Peligroso_gr:     float
+
+    Cant_Mouses_Reciclados: int
+    Cant_Teclados_Reciclados: int
+    Cant_Mouses_Reutilizados: int
+    Cant_Teclados_Reutilizados: int
+    
     Perifericos_Reciclados:   int
     Perifericos_Reutilizados: int
+
+    Lote: int
+    Cant_Mouses: int
+    Cant_Teclados: int
 
 
 class SimulacionOut(BaseModel):
